813550ef1815c7972ad4f14a3f6f21a7273e3638
kent10000 <aaronrichards4321@gmail.com>
2022-09-09 22:11:53 -0400

    Initial commit

---
+++ SongSort/Program.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using Id3;
+
+#endregion
+
+const string directory = @"C:\Users\kent\Music\jd";
+var files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
+
+foreach (var file in files)
+{
+    Id3Tag[] tags;
+    using (var d = new Mp3(file))
+    {
+        tags = d.GetAllTags().ToArray();
+    }
+
+    foreach (var tag in tags)
+        if (tag.Genre.IsAssigned && tag.Genre != "ÿ")
+        {
+            var gen = tag.Genre.Value.Replace('\0', '-'); //replace null chars with random char
+            var newPath = Path.Combine(directory, gen, Path.GetFileName(file));
+
+
+            try
+            {
+                File.Move(file, newPath);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(Path.Combine(directory, gen));
+                File.Move(file, newPath);
+            }
+
+
+            Console.WriteLine($"Moved {file} to" + Path.Combine(directory, tag.Genre, Path.GetFileName(file)));
+
+            break;
+        }
+}
